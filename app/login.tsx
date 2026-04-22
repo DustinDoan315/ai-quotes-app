a7d3647fbea38eb236a7d38d146e7303b1247785
DustinDoan315 <doankhanhdong3105@gmail.com>
2026-04-22 13:46:44 +0700

    chore: move Google OAuth client IDs to env vars

--- app/login.tsx
+++ app/login.tsx
@@ -16,8 +16,8 @@ import {
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { goBackOrReplace } from "@/utils/goBackOrReplace";
 
-const IOS_CLIENT_ID = "744209131746-b05bu8ei1gqsa7s62b8ve4v2re3n3dsd.apps.googleusercontent.com";
-const WEB_CLIENT_ID = "744209131746-c948eqrvvencbe2ijqu5qjtf4f5m39li.apps.googleusercontent.com";
+const IOS_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID ?? "";
+const WEB_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID ?? "";
 
 GoogleSignin.configure({
   iosClientId: IOS_CLIENT_ID,
